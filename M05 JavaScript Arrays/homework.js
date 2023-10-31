263649db3855bfc362e8eb3b5f52765db3414d59
Erick Gonda <egonda1992@gmail.com>
2023-10-30 19:19:30 -0500

    termine el modulo 5, pero el ultimo ejercicio extra me sale error en el testeo, aunque el codigo considero que si esta bien

--- M05 JavaScript Arrays/homework.js
+++ M05 JavaScript Arrays/homework.js
@@ -81,6 +81,7 @@ function arrayContiene(array, elemento) {
 let frutas = ["pera","manzana","frejol"];
 let buscar = arrayContiene(frutas,'pera');
 console.log(buscar);
+
 /*
 function agregarNumeros(arrayOfNums) {
    // El parámetro "arrayOfNums" debe ser un arreglo de números.
@@ -130,62 +131,126 @@ arrayOfNums = [12,43,2,54,6,76];
 console.log(numeroMasGrande(arrayOfNums));
 
 
-function multiplicarArgumentos(arguments) {
+function multiplicarArgumentos() {
    // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto.
    // Si no se pasan argumentos retorna 0. Si se pasa un argumento, simplemente retórnalo.
    // [PISTA]: "arguments" es un arreglo.
    //arguments.length***
-   if(arguments.length > 1){
-      let resultado = arguments.reduce(function(producto, numero){
-         return producto * numero
-      },1)
-      return resultado;
-   } else if (arguments.length == 1){
-      return arguments * 1;
-   }else {
+   let multiplicados = 1;
+   if(arguments.length === 0){
       return 0
+   } if (arguments.length == 1){
+      return arguments[0];
    }
+      for(i=0;i<arguments.length;i++){
+      multiplicados = multiplicados * arguments[i]
+     }
+     return multiplicados;
 }
-arguments = [2,4,6];
-console.log(multiplicarArgumentos(arguments));
+console.log(multiplicarArgumentos());
 
 
 function cuentoElementos(array) {
    // Desarrolla una función que retorne la cantidad de elementos del arreglo cuyo valor sea mayor que 18.
-   // Tu código:
+   let mayorA18 = array.filter(extraerNumeros => extraerNumeros > 18);
+   return mayorA18.length;
 }
+array = [2,4,6,20,22,24,26,8,10,12,14];
+console.log(cuentoElementos(array));
+
 
 function diaDeLaSemana(numeroDeDia) {
    // Supongamos que los días de la semana se codifican como 1 = Domingo, 2 = Lunes y así sucesivamente.
    // Realiza una función que, dado el número del día de la semana, retorne: "Es fin de semana"
    // si el día corresponde a "Sábado" o "Domingo", y "Es dia laboral" en caso contrario.
-   // Tu código:
+   if (numeroDeDia === 2) {
+      return `Es dia laboral`;
+  } else if (numeroDeDia === 3) {
+      return "Es dia laboral";
+  }else if (numeroDeDia === 4) {
+      return "Es dia laboral";
+  }else if (numeroDeDia === 5) {
+      return "Es dia laboral";
+  }else if (numeroDeDia === 6) {
+      return "Es dia laboral";
+  } else {
+      return "Es fin de semana";
+  }
 }
+console.log(diaDeLaSemana(1));
+
 
 function empiezaConNueve(num) {
    // Esta función recibe por parámetro un número.
    // Debe retornar true si el entero inicia con 9 y false en otro caso.
-   // Tu código:
+   num ;
+   let aString = num.toString();
+   if(aString.startsWith("9")){
+      return true
+   } else {
+      return false
+   }
+
 }
+console.log(empiezaConNueve(999898))
+
 
 function todosIguales(array) {
    // Si todos los elementos del arreglo son iguales, retornar true.
    // Caso contrario retornar false.
-   // Tu código:
+   let numerosIguales = array.every(function(valores,indice,array){
+      return valores === array[0];
+   })
+   return numerosIguales;
 }
+array = [2,2,2,2];
+console.log(todosIguales(array));
 
 function mesesDelAño(array) {
    // El arreglo contiene algunos meses del año desordenados. Debes recorrerlo, buscar los meses "Enero",
    // "Marzo" y "Noviembre", guardarlos en un nuevo arreglo y retornarlo.
    // Si alguno de los meses no está, retornar el string: "No se encontraron los meses pedidos".
-   // Tu código:
+   let mesesEncontrados = [];
+
+   for (i=0; i<array.length; i++) {
+       if(array[i] === "Enero" || array[i] === "Marzo" || array[i] === "Noviembre"){
+           mesesEncontrados.push(array[i]);
+       }
+   }
+   if(mesesEncontrados.length === 3){
+       return mesesEncontrados;
+   } else {
+       return "No se encontraron los meses pedidos"
+   }
 }
+console.log(mesesDelAño(["Marzo","Noviembre","Enero"]))
+
 
 function tablaDelSeis() {
    // Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
    // La función devuelve un arreglo con los resultados de la tabla de multiplicar del 6 en orden creciente.
-   // Tu código:
+   let resultadoDeLaMultiplicacion = [];
+   let multiplicador = 6;
+   for(i=0; i<=10; i++){
+      let result = multiplicador * i;
+      resultadoDeLaMultiplicacion.push(result)
+   }
+   return resultadoDeLaMultiplicacion;
 }
+console.log(tablaDelSeis());
+
+/*otra manera de resolverlo
+function tablaDelSeis(numero) {
+   let resultadoDeLaMultiplicacionDe6 = [];
+   for(i=0; i<=10; i++){
+       let resultado = numero * i;
+       resultadoDeLaMultiplicacionDe6.push(resultado);
+   }
+   return resultadoDeLaMultiplicacionDe6;
+}
+numeros = 6;
+let rspta = tablaDelSeis(numeros);
+console.log(rspta); */
 
 function mayorACien(array) {
    // La función recibe un arreglo con enteros entre 0 y 200.
@@ -227,17 +292,36 @@ function breakStatement(num) {
    // Si en algún momento el valor de la suma y la cantidad de iteraciones coinciden, debe interrumpirse
    // la ejecución y retornar el string: "Se interrumpió la ejecución".
    // [PISTA]: utiliza el statement 'break'.
-   // Tu código:
+   let arrayAcumulador = [];
+   for(i=0; i<10; i++){
+      num = num + 2;
+      if(num === 10){
+         return "Se interrumpió la ejecución";
+      } else{
+         arrayAcumulador.push(num)
+      }
+   }
+   return arrayAcumulador;
 }
+console.log(breakStatement(10)); 
+
 
 function continueStatement(num) {
    // Iterar en un bucle aumentando en 2 el número recibido hasta un límite de 10 veces.
    // Guardar cada nuevo valor en un array y retornarlo.
-   // Cuando el número de iteraciones alcance el valor 5, no se suma ese caso y
-   // se continua con la siguiente iteración.
+   // Cuando el número de iteraciones alcance el valor 5, no se suma ese caso y se continua con la siguiente iteración.
    // [PISTA]: utiliza el statement 'continue'.
-   // Tu código:
+   let arrayAcumulador = [];
+   for(i=0; i<10; i++){
+      num = num + 2;
+      if(i === 4){
+         continue;
+      }
+      arrayAcumulador.push(num);
+   }
+   return arrayAcumulador;
 }
+console.log(continueStatement(50));
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
 module.exports = {
